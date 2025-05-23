5225c6dc7c7651edb397d19657436273960f9c5d
Inés Pérez <145657314+inespv@users.noreply.github.com>
2025-05-23 13:45:34 +0200

    ojo inicio sesión

--- resources/views/iniciosesion.blade.php
+++ resources/views/iniciosesion.blade.php
@@ -19,17 +19,47 @@
                     $password = $_REQUEST['contraseña'];
                 }
                 else 
-                {
+                {9
                     print("usuario incorrecto");
                 }*/
             ?>
             <br>
             <p>Introduce tu contraseña:</p>
-            <input type = "password" name = "password" size = "10" placeholder ="tu contraseña" class="password">
-            <br>
-            <input type="submit" value="Iniciar sesión">  
-            <input type="reset" value="Borrar"> 
-           </form>
+            <input type = "password" name = "password" size = "10"
+             placeholder ="tu contraseña" id="password">
+
+            <div class="ojo">
+                  <button type="button" onclick="togglePassword()">
+                    <img id="toggle-icon" src="ojo_abierto-removebg-preview.png" alt="Mostrar">
+                </button>
+            </div>
+
+            <script>
+                function togglePassword() 
+                {
+                    "click", function(event)
+                    {
+                        let passwordField = document.getElementById("password");
+                        let toggleIcon = document.getElementById("toggle-icon");
+
+                        if (passwordField.type === "password") 
+                        {
+                            passwordField.type = "password";
+                            toggleIcon.src = "ojo_cerrado-removebg-preview.png";
+                            toggleIcon.alt = "Ocultar";
+                        } 
+                        else
+                        {
+                            passwordField.type = "password";
+                            toggleIcon.src = "ojo_abierto-removebg-preview.png";
+                            toggleIcon.alt = "Mostrar";
+                        }           
+                    }
+                }
+            </script>
+        <input type="submit" value="Iniciar sesión">  
+        <input type="reset" value="Borrar"> 
+    </form>
     </div>
 </body>
 </html>
